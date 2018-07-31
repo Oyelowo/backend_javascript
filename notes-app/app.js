c5a9d29cc1e46fab10d15ab6ac21b1cb9fee8022
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-07-31 18:05:48 +0300

    Initial commit

---
+++ notes-app/app.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+// console.log(notes.addNote());
+console.log(notes.multiplyAll(1,2,3, 5, 11,34,3));
+
+
+// username = os.userInfo().username;
+// console.log(username);
+
+// console.log('starting app');
+// fs.appendFile('sayHi.txt', `Hello${username}!`, (err) => {
+//     err
+//         ?
+//         console.log('something went wrong') :
+//         console.log('App working');
+// });
+
---
+++ notes-app/notes.js
@@ -0,0 +1,13 @@
+console.log('starting notes.js');
+
+console.log(module);
+
+module.exports.addNote = () => {
+    console.log("let's add something");
+    return 'some notes';
+}
+
+module.exports.multiplyAll = (...args) => {
+    let result = args.reduce((acc, val) => acc * val);
+    return result;
+}
