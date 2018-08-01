002e1df4645180fb4dcbefabb34a7ad1efbb20b7
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-01 16:04:39 +0300

    trying out lodash

--- notes-node/app.js
+++ notes-node/app.js
@@ -6,7 +6,7 @@ const notes = require('./notes.js');
 // console.log(notes.addNote());
 console.log(notes.multiplyAll(1, 2, 3, 5, 11, 34, 3));
 
-duplicateArray = [2, 5, 5, 4, "Oyelowo", "oyelowo", "Oyelowo", 345, 2, 8, 5];
+duplicateArray = ['tt',2, 5, 5, 4, "Oyelowo", "oyelowo", "Oyelowo", 345, 2, 8, 5];
 console.log(_.uniq(duplicateArray));
 
 // username = os.userInfo().username;
