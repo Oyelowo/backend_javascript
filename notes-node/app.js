48c0b115b13bdee583aa5b4c32bc83d14f10ce3b
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-01 17:11:10 +0300

    using yargs

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,16 +1,21 @@
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
 command = process.argv[2];
 console.log('command:', command);
 
+argv = yargs.argv;
+console.log('yargs', argv)
+
 console.log(process.argv);
 
 switch (command) {
     case 'add':
         console.log('adding new notes');
+        notes.addNote(argv.title, argv.body);
         break;
     case 'list':
         console.log('listing notes');
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -2,12 +2,12 @@ console.log('starting notes.js');
 
 // console.log(module);
 
-module.exports.addNote = () => {
-    console.log("let's add something");
-    return 'some notes';
+module.exports.addNote = (title, body) => {
+    console.log('adding note', title, body);
 }
 
 module.exports.multiplyAll = (...args) => {
     let result = args.reduce((acc, val) => acc * val);
     return result;
 }
+
