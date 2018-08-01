bfdd11d54f5b4830db1aba89203e2e125955c0cd
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-01 22:49:17 +0300

    functions created for fetching and saving notes

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -3,23 +3,32 @@ const fs = require('fs');
 
 // console.log(module);
 
-const addNote = (title, body) => {
-    let notes = [];
-    let note = {
-        title,
-        body
-    };
-
+const fetchNotes = () => {
     // use try incase of the first time when the json file has not been created
     // this would prevent error.
     try {
         noteString = fs.readFileSync('notes-data.json');
         notes = JSON.parse(noteString);
-    } catch (e) {}
+    } catch (e) {
+        notes = [];
+    }
+    return notes;
+}
+
+const saveNotes = (notes) => fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+
+const addNote = (title, body) => {
+    let notes = fetchNotes();
+    let note = {
+        title,
+        body
+    };
+    fetchNotes();
 
     if (!notes.find(note => note.title === title)) {
         notes.push(note);
-        fs.writeFileSync("notes-data.json", JSON.stringify(notes))
+        saveNotes(notes);
+        console.log("Note created!");
     } else {
         console.log("NOTE ALREADY EXIST");
     }
