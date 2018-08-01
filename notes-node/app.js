395652c15c60156e7963eb1751314ed3ecf0deba
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-01 16:38:35 +0300

    more conditions added to the switch statement

--- notes-node/app.js
+++ notes-node/app.js
@@ -6,12 +6,20 @@ const notes = require('./notes.js');
 command = process.argv[2];
 console.log('command:', command);
 
+console.log(process.argv);
+
 switch (command) {
     case 'add':
         console.log('adding new notes');
         break;
     case 'list':
         console.log('listing notes');
+        break;
+    case 'read':
+        console.log('reading notes');
+        break;
+    case 'remove':
+        console.log('Removed notes');
         break;
     default:
         console.log('command not recognised');
