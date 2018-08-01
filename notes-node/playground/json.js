ff32021ad02839e7b6a86eaf141c4703966b008a
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-01 17:38:59 +0300

    parsing a json string which could be received from an API using JSON.parse

--- notes-node/playground/json.js
+++ notes-node/playground/json.js
@@ -5,3 +5,10 @@ let obj = {
 let stringObj = JSON.stringify(obj);
 console.log(typeof stringObj);
 console.log(stringObj);
+
+
+let personString = '{"name": "Oyelowo","Age": "24"}';
+
+let personObj = JSON.parse(personString);
+console.log(typeof personObj);
+console.log(personObj);
