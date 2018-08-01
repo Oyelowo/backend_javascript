f5312b7f6788ba25e745b2a690dafc786f8e7f5b
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-01 17:48:47 +0300

    practising fs.writeFileSync and JSON.parse and JSON.stringify

--- notes-node/playground/json.js
+++ notes-node/playground/json.js
@@ -1,14 +1,33 @@
-let obj = {
-    name: 'Oyelowo'
-}
+// let obj = {
+//     name: 'Oyelowo'
+// }
 
-let stringObj = JSON.stringify(obj);
-console.log(typeof stringObj);
-console.log(stringObj);
+// let stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
 
 
-let personString = '{"name": "Oyelowo","Age": "24"}';
+// let personString = '{"name": "Oyelowo","Age": "24"}';
 
-let personObj = JSON.parse(personString);
-console.log(typeof personObj);
-console.log(personObj);
+// let personObj = JSON.parse(personString);
+// console.log(typeof personObj);
+// console.log(personObj);
+
+const fs = require('fs');
+
+let originalNote = {
+    title: 'the title',
+    body: 'the body'
+};
+
+// originalNoteString
+let originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+let noteString = fs.readFileSync('notes.json');
+
+// note
+let note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
