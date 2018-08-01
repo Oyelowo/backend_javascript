18a711e8591fe914aeaf8832ac06805260b5c2bf
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-01 16:07:30 +0300

    main file reorgnised

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,23 +1,4 @@
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-// console.log(notes.addNote());
-console.log(notes.multiplyAll(1, 2, 3, 5, 11, 34, 3));
-
-duplicateArray = ['tt',2, 5, 5, 4, "Oyelowo", "oyelowo", "Oyelowo", 345, 2, 8, 5];
-console.log(_.uniq(duplicateArray));
-
-console.log(_.isString(duplicateArray));
-console.log(_.isString("duplicateArray"));
-// username = os.userInfo().username;
-// console.log(username);
-
-// console.log('starting app');
-// fs.appendFile('sayHi.txt', `Hello${username}!`, (err) => {
-//     err
-//         ?
-//         console.log('something went wrong') :
-//         console.log('App working');
-// });
