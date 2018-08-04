4f53696be9c17bb28e93cbddff635e23c79a696f
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-04 18:45:41 +0300

    methods in in object

--- notes-node/playground/arrow-function.js
+++ notes-node/playground/arrow-function.js
@@ -1,3 +1,13 @@
 let square = x => x * x;
 
-console.log(square(9))
+console.log(square(9));
+
+let student = {
+    name: 'Oyelowo',
+    sayHello(){
+        console.log(`Hello, I am ${this.name}`);
+    }
+};
+
+
+student.sayHello();
