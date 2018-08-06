422c240f8f8e263a82dea4dfd823d7ea7c09247b
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-06 20:50:16 +0300

    fetching data

--- weather-app/playground/app.js
+++ weather-app/playground/app.js
@@ -1,11 +1,10 @@
 const request = require('request');
 
 request({
-    url: 'https://mapsgoogleapis.com/maps/api/geocode/json?address=1301%20lombard%20street' +
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street' +
             '%20philidelphia&key=AIzaSyAr1xwsViJwAjAwTEMIjxbCRjdg-6LkEdw',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(error, undefined, 4));
+    console.log(JSON.stringify(body.results[0].address_components, undefined, 4));
 });
- 
 
