0fa47e6448dfb81659c90c5411e68aba98b14bf6
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-06 20:46:03 +0300

    error handling

--- weather-app/playground/app.js
+++ weather-app/playground/app.js
@@ -1,10 +1,11 @@
 const request = require('request');
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street' +
+    url: 'https://mapsgoogleapis.com/maps/api/geocode/json?address=1301%20lombard%20street' +
             '%20philidelphia&key=AIzaSyAr1xwsViJwAjAwTEMIjxbCRjdg-6LkEdw',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 4));
+    console.log(JSON.stringify(error, undefined, 4));
 });
+ 
 
