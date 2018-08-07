3647e1af4fa36296554a592620eff66129dc2004
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-07 21:35:34 +0300

    convert temperature from fahreheit to celsius

--- weather-app/getWeather/getWeather.js
+++ weather-app/getWeather/getWeather.js
@@ -1,5 +1,7 @@
 const request = require('request');
 
+const fahrToCels = fahrTemp => ((fahrTemp - 32) * 5 / 9).toFixed(2);
+
 getWeather = (lat, long, callback) => {
     request({
         url: `https://api.darksky.net/forecast/be243b78b4c7c873806b3726863d5ee3/${lat},${long}`,
@@ -7,8 +9,8 @@ getWeather = (lat, long, callback) => {
     }, (error, response, body) => {
         if (!error && response.statusCode === 200) {
             callback(null, {
-                temperature: body.currently.temperature,
-                apparentTemperature: body.currently.apparentTemperature
+                temperature: fahrToCels(body.currently.temperature) + ' °C',
+                apparentTemperature: fahrToCels(body.currently.apparentTemperature) + ' °C'
             });
         } else {
             callback('unable to fetch weather')
