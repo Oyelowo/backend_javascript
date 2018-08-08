ab103e28f5fdbdb1b92f989163218aec19080685
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-08 20:17:58 +0300

    more on express

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
-});
+    res.send('<h1>Hello Express!</h1>');
+}); 
 
 app.listen(3000);
