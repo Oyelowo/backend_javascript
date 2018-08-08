c4aa18883147422e924947601136c7d7b3a50b25
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-08 05:16:05 +0300

    more  on promises

--- weather-app/playground/promise.js
+++ weather-app/playground/promise.js
@@ -1,3 +1,15 @@
+let asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            } else {
+                reject('Arguments must  be numbers');
+            }
+        }, 2000);
+    })
+}
+
 let myPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve('Hey Oyelowo!, it worked!');
