273869fb4c64ec0dab5af0f5030e47010d6af04a
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-08 20:12:40 +0300

    minor fix

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-    req.setEncoding('Hello Express!');
+    res.send('Hello Express!');
 });
 
 app.listen(3000);
