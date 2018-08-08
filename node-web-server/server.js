5d50f3fe0d994145c86bf45f2c32be7cafc8b841
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-08 20:20:35 +0300

    sending json data to the client via express

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,7 +3,11 @@ const express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello Express!</h1>');
-}); 
+    // res.send('<h1>Hello Express!</h1>');
+    res.send({
+        name: 'Oyelowo',
+        likes: ['Coding', 'Traveling']
+    })
+});
 
 app.listen(3000);
