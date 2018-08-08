8a1d24b4c6a0eef3db50c86e9eab579f40085166
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-08 06:49:12 +0300

    fetching weather using promises chaining. Location chained with weather fetching

--- weather-app/app-promise.js
+++ weather-app/app-promise.js
@@ -1,5 +1,6 @@
 const yargs = require('yargs');
 const axios = require('axios');
+const fahrToCels = fahrTemp => ((fahrTemp - 32) * 5 / 9).toFixed(2);
 
 const argv = yargs.options({
         a: {
@@ -20,7 +21,16 @@ axios.get(geocodeURL).then((response) => {
     if (response.data.status === "ZERO_RESULTS") {
         throw new Error('unable to find the address');
     }
-    console.log(response.data)
+
+    let [lat, long] = [response.data.results[0].geometry.location.lat, response.data.results[0].geometry.location.lat];
+    let weatherURL = `https://api.darksky.net/forecast/be243b78b4c7c873806b3726863d5ee3/${lat},${long}`
+
+    console.log(response.data.results[0].formatted_address);
+    return axios.get(weatherURL);
+}).then((response) => {
+    let temperature = fahrToCels(response.data.currently.temperature);
+    let apparentTemperature = fahrToCels(response.data.currently.apparentTemperature);
+    console.log(`The Temperature currently is ${temperature} and feels like ${apparentTemperature}`);
 }).catch((error) => {
     if (error.code === "ENOTFOUND") {
         console.log("unable to connect to API server");
