fc04bdc02a12e0ca1b329896fa347c6d4c1cda7a
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-09 19:40:11 +0300

    reorganising the commands such that files in the public are placed below where maintenance is rendered in the middleware. this makes it impossible to access the help page

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -9,7 +9,6 @@ hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 hbs.registerHelper('shoutIt', (text) => text.toUpperCase());
 
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
     let now = new Date().toString();
@@ -26,6 +25,8 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
     res.render('maintenance.hbs', {maintenanceMsg: 'Coming  Soon'});
 });
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => { //
     res.send('<h1>Hello Express!</h1>');
