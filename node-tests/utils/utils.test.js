c1246c87f107193dfe81ccedaaadd9e916fb3f9a
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-11 21:12:12 +0300

    using the expect library

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,10 +1,12 @@
+const expect = require('expect');
 const utils = require('./utils')
 
 it('should add two numbers', () => {
     let res = utils.add(24, 22);
-    if (res !== 46) {
-        throw new Error(`Expected 44 but got ${res}`);
-    }
+    expect(res).toBe(46);
+    // if (res !== 46) {
+    //     throw new Error(`Expected 44 but got ${res}`);
+    // }
 });
 
 it('should square a number', () => {
