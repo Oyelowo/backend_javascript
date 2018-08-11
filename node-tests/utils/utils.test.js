89c49a732a735a19bd0e3b8606a31b52f2187735
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-11 20:51:47 +0300

    more

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -2,4 +2,7 @@ const utils = require('./utils')
 
 it('should add two numbers', () => {
     let res = utils.add(24, 22);
+    if (res !== 46) {
+        throw new Error(`Expected 44 but got ${res}`);
+    }
 });
