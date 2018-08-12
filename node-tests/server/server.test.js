c21e24f521ff3cfb85f3bf0a337fc2fbe2f5feb0
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-12 07:58:29 +0300

    using nested describe in the express app

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -5,32 +5,47 @@ const express = require('express');
 
 let app = require('./server').app;
 
-it('should return Hello World! response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found'
-            });
-        })
-        // .expect({ 
-        //     error: 'Page not found'
-        // })
-        // .expect('Hello World!')
-        .end(done);
+//server
+//GET /
+//some test case
+describe('GET /', () => {
+    it('should return Hello World! response', (done) => {
+        request(app)
+            .get('/')
+            .expect(404)
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    error: 'Page not found'
+                });
+            })
+            // .expect({ 
+            //     error: 'Page not found'
+            // })
+            // .expect('Hello World!')
+            .end(done);
+    });
 });
 
-//Make a new test
-//assert 200
-// assert that I exist in users' array
-it('gets arrays of users and returns my object', (done)=>{
-request(app)
-.get('/users')
-.expect(200)
-.expect((res)=>{
-    expect(res.body).toBeA('array');
-    expect(res.body).toInclude({name: 'Oyelowo', age: 24});
-})
-.end(done);
+
+//GET /users
+//some test case
+describe('server', () => {
+    describe('GET /users', () => {
+        //Make a new test
+        //assert 200
+        // assert that I exist in users' array
+        it('gets arrays of users and returns my object', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toBeA('array');
+                    expect(res.body).toInclude({
+                        name: 'Oyelowo',
+                        age: 24
+                    });
+                })
+                .end(done);
+        });
+    });
 })
