d2064b6bbeafc921eaac980cb416175cc20e919c
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-12 07:10:51 +0300

    combining mocha, supertest  and mjackson's expect for testing the app

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -4,7 +4,8 @@ let app = express();
 
 app.get('/', (req, res) => {
     res.status(404).send({
-        error: 'Page not found'
+        error: 'Page not found',
+        name: 'Todo Aoo v1.0'
     });
 });
 
--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const expect = require('expect');
 const express = require('express');
 
 
@@ -8,9 +9,14 @@ it('should return Hello World! response', (done) => {
     request(app)
         .get('/')
         .expect(404)
-        .expect({ 
-            error: 'Page not found'
+        .expect((res) => {
+            expect(res.body).toInclude({
+                error: 'Page not found'
+            });
         })
+        // .expect({ 
+        //     error: 'Page not found'
+        // })
         // .expect('Hello World!')
         .end(done);
 })
