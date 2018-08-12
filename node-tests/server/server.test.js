75819ea52bddb0a24fe730e2080ddd07c8532e2a
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-12 07:07:14 +0300

    expect json

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -7,7 +7,10 @@ let app = require('./server').app;
 it('should return Hello World! response', (done) => {
     request(app)
         .get('/')
-        .expect(200)
-        .expect('Hello World!')
+        .expect(404)
+        .expect({
+            error: 'Page not found'
+        })
+        // .expect('Hello World!')
         .end(done);
 })
