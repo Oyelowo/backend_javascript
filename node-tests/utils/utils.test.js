f88ea722c24a0d8759146182eb60fb74184b5f89
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-12 07:52:54 +0300

    nesting groups of test with similar methods using describe method

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -2,35 +2,43 @@ const expect = require('expect');
 const utils = require('./utils');
 
 describe('utils', () => {
-    it('should add two numbers', () => {
-        let res = utils.add(24, 22);
-        expect(res).toBe(46).toBeA('number');
-        // if (res !== 46) {
-        //     throw new Error(`Expected 44 but got ${res}`);
-        // }
-    });
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            let res = utils.add(24, 22);
+            expect(res).toBe(46).toBeA('number');
+            // if (res !== 46) {
+            //     throw new Error(`Expected 44 but got ${res}`);
+            // }
+        });
 
-    it('should async add two numbers', (done) => {
-        utils.asyncAdd(2, 3, (sum) => {
-            expect(sum).toBe(5).toBeA('number');
-            done();
+        it('should async add two numbers', (done) => {
+            utils.asyncAdd(2, 3, (sum) => {
+                expect(sum).toBe(5).toBeA('number');
+                done();
+            });
         });
     });
 
-    it('should square a number', () => {
-        let res = utils.square(5);
-        expect(res).toBe(25).toBeA('number');
-        // if (res !== 25) {
-        //     throw new Error(`Expected 25 but got ${res}`);
-        // }
-    });
+    describe('#square', () => {
+        it('should square a number', () => {
+            let res = utils.square(5);
+            expect(res).toBe(25).toBeA('number');
+            // if (res !== 25) {
+            //     throw new Error(`Expected 25 but got ${res}`);
+            // }
+        });
 
-    it('should async square a number', (done) => {
-        utils.asyncSquare(5, (squared) => {
-            expect(squared).toBe(25).toBeA('number');
-            done();
+        it('should async square a number', (done) => {
+            utils.asyncSquare(5, (squared) => {
+                expect(squared).toBe(25).toBeA('number');
+                done();
+            });
         });
     });
+
+
+
+
 });
 
 
