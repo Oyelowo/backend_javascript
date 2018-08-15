fd03716ec213a1793ae761244138d78801a2b7e2
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-15 17:46:58 +0300

    handling error in database

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,1 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('unable to connect to mongodb server');
+    }
+    console.log('connected to mongodb server')
+})
