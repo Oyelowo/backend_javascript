86f9b5b2818c86cd2c5e15846071dfd4f00e5b2c
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-15 18:22:31 +0300

    getting timestamp from _id property in mongodb

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -27,8 +27,8 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
         if (err) {
             return console.log('Unable to insert user', err);
         }
-        console.log(JSON.stringify(result.ops, null, 2));
-    })
+        console.log(result.ops[0]._id.getTimestamp());
+    });
 
     client.close();
 })
