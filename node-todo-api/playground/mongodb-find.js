b5bfd5fce5a42864862a78490cefb7495edcb2bc
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-15 20:34:51 +0300

    querying documents

--- node-todo-api/playground/mongodb-find.js
+++ node-todo-api/playground/mongodb-find.js
@@ -15,7 +15,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('connected to Mongodb server');
     const db = client.db('TodoApp');
 
-    db.collection('Todos').find().toArray().then((docs) => {
+    db.collection('Todos').find({completed: true}).toArray().then((docs) => {
         console.log('Todos\n', JSON.stringify(docs, null, 2));
     }, (err) => {
         console.log('Unable to fetch todos', err);
