0f1ce8134ab7a1c1c88d9be9905288ed2f26a518
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-15 20:41:22 +0300

    count method of db collection cursor

--- node-todo-api/playground/mongodb-find.js
+++ node-todo-api/playground/mongodb-find.js
@@ -15,10 +15,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('connected to Mongodb server');
     const db = client.db('TodoApp');
 
-    db.collection('Todos').find({
-        _id: new ObjectID('5b74405b9362004fa0f4f86d')
-    }).toArray().then((docs) => {
-        console.log('Todos\n', JSON.stringify(docs, null, 2));
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5b74405b9362004fa0f4f86d')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos\n', JSON.stringify(docs, null, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos count: ${count}`);
     }, (err) => {
         console.log('Unable to fetch todos', err);
     });
