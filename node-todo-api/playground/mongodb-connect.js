a56dffb4174bbfc86b57f455cc5c7caaeb5b0809
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-15 18:10:41 +0300

    inserting user into users db

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -7,13 +7,28 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('connected to Mongodb server');
     const db = client.db('TodoApp');
 
-    db.collection('Todos').insertOne({
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to leave',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, null, 2));
+    // });
 
+    //insert one doc into users (name, age, location)
+
+    db.collection('Users').insertOne({
+        name: 'Oyelowo Oyedayo',
+        age: 24,
+        location: 'Helsinki, Finland'
     }, (err, result) => {
         if (err) {
-            return console.log('Unable to insert todo', err);
+            return console.log('Unable to insert user', err);
         }
         console.log(JSON.stringify(result.ops, null, 2));
     })
+
     client.close();
 })
