0181f08d6ea72e3ee042e137483c6c654be0c7b8
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-15 23:10:18 +0300

    deleting docs

---
+++ node-todo-api/playground/mongodb-delete.js
@@ -0,0 +1,27 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
+
+
+let obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('unable to connect to mongodb server');
+    }
+    console.log('connected to Mongodb server');
+    const db = client.db('TodoApp');
+
+    // deleteMany
+    db.collection('Todos').deleteMany({
+        text: 'Something to do'
+    }).then(docs => console.log(docs))
+    // deleteOne
+
+    // findOneDelete
+
+    // client.close();
+})
