152a1b7b90d37b3f1afa65a8f737a1d14abf90da
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-16 21:48:54 +0300

    deleting mutiple documents practise

--- node-todo-api/playground/mongodb-delete.js
+++ node-todo-api/playground/mongodb-delete.js
@@ -1,5 +1,8 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectID} = require('mongodb');
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
 
 let obj = new ObjectID();
 console.log(obj);
@@ -12,14 +15,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     const db = client.db('TodoApp');
 
     // deleteMany db     .collection('Todos')     .deleteMany({text: 'Something to
-    // do'})     .then(docs => console.log(docs)); deleteOne db
-    // .collection('Todos')     .deleteOne({text: 'Read the book'})     .then(docs
-    // => console.log(docs)); findOneDelete
+    // do'})     .then(docs => console.log(docs)); deleteOne db .collection('Todos')
+    //     .deleteOne({text: 'Read the book'})     .then(docs => console.log(docs));
+    // findOneDelete db     .collection('Todos')     .findOneAndDelete({completed:
+    // false})     .then(result => console.log(result)); 
 
-    db
-        .collection('Todos')
-        .findOneAndDelete({completed: false})
-        .then(result => console.log(result));
+    db.collection('Users').deleteMany({
+        name: 'Oyelowo Oyedayo'
+    }).then(docs => console.log(docs));
+
 
     // client.close();
 
