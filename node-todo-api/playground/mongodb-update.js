69c5520aeb748ddd01eade20184577aac0c87cd7
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-17 06:42:54 +0300

    setting and incrementing in findOneAndUpdate

--- node-todo-api/playground/mongodb-update.js
+++ node-todo-api/playground/mongodb-update.js
@@ -14,15 +14,31 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('connected to Mongodb server');
     const db = client.db('TodoApp');
 
-    db.collection('Todos').findOneAndUpdate({
-        _id: new ObjectID("5b743f512ed1044c846ba0f7")
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID("5b743f512ed1044c846ba0f7")
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then(doc => console.log(doc))
+
+
+    db.collection('Users').findOneAndUpdate({
+        _id: ObjectID("5b7441bf0a354a3ddcccb158")
     }, {
         $set: {
-            completed: true
+            name: 'Oyelowo'
+        },
+
+        $inc: {
+            age: 200
         }
     }, {
         returnOriginal: false
-    }).then(doc => console.log(doc))
+    }).then(doc => console.log(doc), err => console.log(err));
+
 
     // client.close();
 
