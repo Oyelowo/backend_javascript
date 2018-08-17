26b3f854f3f36c533e8e8e7e6a00b30e4d957512
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-17 07:05:25 +0300

    setting inbuilt promise

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,4 +1,6 @@
 let mongoose = require('mongoose');
+// to make it use inbuilt promise as opposed to third party Promise
+mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
