a2874a97cbd0962f06d09e46b6ed827a0cb438c5
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-19 07:39:48 +0300

    setting up a post request to todo

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -11,3 +11,14 @@ let {
 let {
     User
 } = require('./models/user');
+
+
+let app = express();
+
+app.post('/todo', (req, res)=>{
+
+});
+
+app.listen(3000, ()=>{
+    console.log('start on port 3000');
+})
