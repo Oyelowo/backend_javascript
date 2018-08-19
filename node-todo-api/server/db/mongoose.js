2037bd45c90258c8155f9e8ee3148112dde9db1a
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-19 07:22:28 +0300

    putting mongoose setup in another file

---
+++ node-todo-api/server/db/mongoose.js
@@ -0,0 +1,9 @@
+let mongoose = require('mongoose');
+// to make it use inbuilt promise as opposed to third party Promise
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+module.exports.mongoose={
+    mongoose
+}
--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,8 +1,3 @@
-let mongoose = require('mongoose');
-// to make it use inbuilt promise as opposed to third party Promise
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 // save new something
 let Todo = mongoose.model('Todo', {
