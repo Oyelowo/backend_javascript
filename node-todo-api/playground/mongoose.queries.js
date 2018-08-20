e8729958c5c53b46418124cf14b1ab5673ff71a0
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-20 09:58:13 +0300

    dealing with id not found by using the ObjectID's isValid method

--- node-todo-api/playground/mongoose.queries.js
+++ node-todo-api/playground/mongoose.queries.js
@@ -1,27 +1,38 @@
 const {
+    ObjectID
+} = require('mongodb');
+
+const {
     mongoose
 } = require('./../server/db/mongoose');
 const {
     Todo
 } = require('./../server/models/todos');
 
-let id = '5b7a60fc97fbad0b98836d44';
 
-Todo.find({
-    _id: id
-}).then((todos) => {
-    console.log('Todos', todos);
-});
 
-Todo.findOne({
-    _id: id
-}).then((todo) => {
-    console.log('Todo', todo);
-});
+// let id = '5b7a60fc97fbad0b98836d4';
 
-Todo.findById(id).then((todo) => {
-    if(!todo){
-        return console.log('id not found')
-    }
-    console.log('Todo By id', todo);
-})
+// if(!ObjectID.isValid(id)){
+//     console.log('ID not valid');
+// }
+
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// }).catch((e) => console.log(e));
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('id not found')
+//     }
+//     console.log('Todo By id', todo);
+// })
