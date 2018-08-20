e57d56db9985921be99b835feaa6776ba76e6739
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-20 17:59:51 +0300

    separating testing and devvelopment db folder

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 // to make it use inbuilt promise as opposed to third party Promise
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
     mongoose
