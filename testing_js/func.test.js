617b083655e72dd530e5a459f930cb71d4700976
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-21 22:18:39 +0300

    testing the fizzbuzz using spy jest.fn()

--- testing_js/func.test.js
+++ testing_js/func.test.js
@@ -22,33 +22,31 @@ describe('Hello', () => {
 
 
 describe('Add', () => {
-  it('should add all elements of an array', ()=>{
-      expect(addAll(1,2,3,4,5,5)).toBe(20);
-      expect(addAll(1,2,3,4,5,'5')).toBeNull();
-      expect(addAll(1,2,3,[],5,5)).toBeNull();
-      expect(addAll(1,2,3,4,{},5)).toBeNull();
-      expect(addAll(1,2,3,4,5,5)).toBeTruthy();
-      expect(addAll(1,2,3,4,5,5)).not.toBeFalsy();
-      expect(addAll(1,2,3,4,5,5)).not.toBeNull();
-      expect(typeof addAll(1,2,3,4,5,5)).toBe('number');
-  });
+    it('should add all elements of an array', () => {
+        expect(addAll(1, 2, 3, 4, 5, 5)).toBe(20);
+        expect(addAll(1, 2, 3, 4, 5, '5')).toBeNull();
+        expect(addAll(1, 2, 3, [], 5, 5)).toBeNull();
+        expect(addAll(1, 2, 3, 4, {}, 5)).toBeNull();
+        expect(addAll(1, 2, 3, 4, 5, 5)).toBeTruthy();
+        expect(addAll(1, 2, 3, 4, 5, 5)).not.toBeFalsy();
+        expect(addAll(1, 2, 3, 4, 5, 5)).not.toBeNull();
+        expect(typeof addAll(1, 2, 3, 4, 5, 5)).toBe('number');
+    });
 });
 
 
 describe('GET /https://jsonplaceholder.typicode.com/posts', () => {
-    it('should GET posts', (done)=>{
+    it('should GET posts', (done) => {
         expect(fetchData('https://jsonplaceholder.typicode.com/posts')).not.toBeNull();
         expect(typeof fetchData('https://jsonplaceholder.typicode.com/posts')).toBe('object');
         done();
     });
-  
+
 })
 
 describe('FIZZBUZZ', () => {
-  it('should be called 100 times', () => {
-      expect(fizzbuzz()).toHaveBeenCalledTimes(100)
-  })
+    it('should be called 100 times', () => {
+        fizzbuzz = jest.fn();
+        expect(fizzbuzz).toHaveBeenCalledTimes(100);
+    });
 })
-
-
-
