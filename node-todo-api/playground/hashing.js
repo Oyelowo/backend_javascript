ac8d25726d42d77bb88497a01d5ac99329f480d4
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-25 05:07:09 +0300

    hasing and sorting with crypto.js

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -2,7 +2,28 @@ const {
     SHA256
 } = require('crypto-js');
 
+
 let message = 'This is Oyelowo';
 let hash = SHA256(message).toString();
 console.log('hash', hash);
 
+
+let data = {
+    id: 4
+};
+
+let token = {
+    data,
+    hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+}
+
+token.data.id=5;
+token.hash=SHA256(JSON.stringify(token.data)).toString();
+
+let resultHash= SHA256(JSON.stringify(token.data)+ 'somesecret').toString();
+
+if(resultHash===token.hash){
+    console.log('data was not changed');
+}else{
+    console.log('data was changed, don\'t trust');
+}
