6d8794b1a10fb70d72a9d9d76dedae6df63786ed
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-25 05:07:36 +0300

    getting started with jwt

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,29 +1,32 @@
 const {
     SHA256
 } = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+jwt.sign
+jwt.verify
+
+// let message = 'This is Oyelowo';
+// let hash = SHA256(message).toString();
+// console.log('hash', hash);
 
 
-let message = 'This is Oyelowo';
-let hash = SHA256(message).toString();
-console.log('hash', hash);
+// let data = {
+//     id: 4
+// };
 
+// let token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
 
-let data = {
-    id: 4
-};
+// token.data.id=5;
+// token.hash=SHA256(JSON.stringify(token.data)).toString();
 
-let token = {
-    data,
-    hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-}
+// let resultHash= SHA256(JSON.stringify(token.data)+ 'somesecret').toString();
 
-token.data.id=5;
-token.hash=SHA256(JSON.stringify(token.data)).toString();
-
-let resultHash= SHA256(JSON.stringify(token.data)+ 'somesecret').toString();
-
-if(resultHash===token.hash){
-    console.log('data was not changed');
-}else{
-    console.log('data was changed, don\'t trust');
-}
+// if(resultHash===token.hash){
+//     console.log('data was not changed');
+// }else{
+//     console.log('data was changed, don\'t trust');
+// }
