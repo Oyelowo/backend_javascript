7b68ffcbb8dc3efab81b65906e2cc89fa361bbad
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-26 23:47:29 +0300

    forecast code refactored

--- ass.js
+++ ass.js
@@ -1,32 +1,59 @@
-// const fs = require('fs');
-// let data = require('./posts.json');
-
-// const exponentialSmoothing = (prevWeekSales, prevWeekForcast, alpha) => {
-//     return (alpha * prevWeekSales + (1 - alpha) * prevWeekForcast)
-// }
-
-// const getData = (id, title, data) => {
-//     return data.filter(el => el.id == id && el.title === title);
-// }
-// console.log(getData('6', 'dolorem eum magni eos aperiam quia', data));
+const fs = require('fs');
+let data = require('./posts.json');
 
 
-// console.log(new Date('2018-08-22').toDateString());
 
 
-// const getForecast = (day, productCode, outletCode, alpha, dataset) => {
-//     let resultObj = dataset.filter(el => {
-//         el.day === day && el.productCode === productCode && el.outletCode === outletCode
-//     });
+const exponentialSmoothing = (prevWeekSales, prevWeekForcast, alpha) => {
+    return (alpha * prevWeekSales + (1 - alpha) * prevWeekForcast)
+}
 
 
-//     let {
-//         sales: prevWeekSales,
-//         forecast: prevWeekForcast
-//     } = resultObj;
+const getForecast = (day, productCode, outletCode, alpha, dataset) => {
+    const resultObj = dataset.filter(el => {
+        el.day === day && el.productCode === productCode && el.outletCode === outletCode
+    });
 
-//     let forecast = exponential_smoothing(prevWeekSales, prevWeekForcast);
-//     return forecast;
-// }
+    const {
+        sales: prevWeekSales,
+        forecast: prevWeekForcast
+    } = resultObj;
+
+    let forecast = exponential_smoothing(prevWeekSales, prevWeekForcast, alpha);
+    return forecast;
+}
+
+
+
+
+
+
+const exponentialSmoothing = (prevWeekSales, prevWeekForcast, alpha) => {
+    return (alpha * prevWeekSales + (1 - alpha) * prevWeekForcast)
+}
+
+const getData = (id, title, data) => {
+    return data.filter(el => el.id == id && el.title === title);
+}
+console.log(getData('6', 'dolorem eum magni eos aperiam quia', data));
+
+
+console.log(new Date('2018-08-22').toDateString());
+
+
+const getForecast = (day, productCode, outletCode, alpha, dataset) => {
+    let resultObj = dataset.filter(el => {
+        el.day === day && el.productCode === productCode && el.outletCode === outletCode
+    });
+
+
+    let {
+        sales: prevWeekSales,
+        forecast: prevWeekForcast
+    } = resultObj;
+
+    let forecast = exponential_smoothing(prevWeekSales, prevWeekForcast);
+    return forecast;
+}
 
 
