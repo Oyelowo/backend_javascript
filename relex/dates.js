244c4af711144a0aab3d4f434e9019dc621a1803
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-28 01:50:40 +0300

    more correction

--- relex/dates.js
+++ relex/dates.js
@@ -1,16 +1,17 @@
 let getPrevWeekDay = (day) => {
-    var date = new Date();
-    var dayOfWeek = date.getDay();
+    day = day.toLowerCase();
+    let date = new Date();
+    let dayOfWeek = date.getDay();
     date.setDate(date.getDate() - dayOfWeek);
     // console.log('let', date); 
     // console.log('date', date.toLocaleDateString(), '\n', date.toString());
     let weekDays = ["sunday", "saturday", "friday", "thursday", "wednesday", "tuesday", "monday"];
     if (weekDays.includes(day)) {
         date.setDate(date.getDate() - weekDays.indexOf(day));
-    } else{
-        throw Error('Date is incorrect');
+    } else {
+        throw Error('Date is incorrect. Check your spelling');
     }
     return date.toLocaleDateString()
 }
 
-console.log('getPrevWeekDay', getPrevWeekDay('thursday'));
+console.log('getPrevWeekDay', getPrevWeekDay('thUrsday'));
