251a708b0f8e2fd7b406aa213f0c4539aaf5f062
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-08-28 01:46:07 +0300

    previous week day function implemented

--- relex/dates.js
+++ relex/dates.js
@@ -1,8 +1,16 @@
-var date= new Date();
- var dummy =  date.getDay();
- console.log('dummy', dummy);
- dummy = dummy - 0;
- date.setDate(date.getDate() - dummy );
- console.log("prev week last day : "+ date.getDate());
- date.setDate(date.getDate() - 6);
-console.log("previous week first day : "+ date.getDate());
+let getPrevWeekDay = (day) => {
+    var date = new Date();
+    var dayOfWeek = date.getDay();
+    date.setDate(date.getDate() - dayOfWeek);
+    // console.log('let', date); 
+    // console.log('date', date.toLocaleDateString(), '\n', date.toString());
+    let weekDays = ["sunday", "saturday", "friday", "thursday", "wednesday", "tuesday", "monday"];
+    if (weekDays.includes(day)) {
+        date.setDate(date.getDate() - weekDays.indexOf(day));
+    } else{
+        throw Error('Date is incorrect');
+    }
+    return date.toLocaleDateString()
+}
+
+console.log('getPrevWeekDay', getPrevWeekDay('thursday'));
